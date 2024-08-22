1eb457e7c0ad82dba46c71d349f021c823551296
Maykon <maykontavares2003@hotmail.com>
2024-08-22 19:47:49 -0300

    C++

--- capitulo02/exercicio08/Account.h
+++ capitulo02/exercicio08/Account.h
@@ -12,16 +12,37 @@ class Account
         Account(int balance)
         {
             setBalance(balance);
-        }  
+        } 
+
+        void credit(int value)
+        {
+            setBalance(value + this->balance);
+        }
+
+        void debit(int value)
+        {
+            if(value < 0)
+            {
+                cout << "Value is invalid" << endl;
+            }else if(value > this->balance)
+            {
+                cout << "Debit amount exceeded account balance" << endl;           
+            }else
+            {
+                setBalance(this->balance - value);
+            }
+        }
 
         void setBalance(int balance)
         {
-            if (balance < 0 ){
+            if (balance < 0 )
+            {
                 this->balance = 0;
                 
                 cout << "The Balance is invalid, because the value is fewer than zero" << endl;
 
-            }else{
+            }else
+            {
                 this -> balance = balance;
             }
             
